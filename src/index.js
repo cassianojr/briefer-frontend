e5d9d03ff3416f3874b418bec62e5ce30213f329
CASSIANO JUNIOR <cassiano.jr.2009@hotmail.com>
2019-04-26 10:46:27 -0300

    removed react-alert that causing a app crash

--- src/index.js
+++ src/index.js
@@ -3,21 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { transitions, positions, Provider as AlertProvider } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
-
-const options = {
-	position: positions.BOTTOM_RIGHT,
-	timeout: 5000,
-	offset: '30px',
-	transition: transitions.SCALE
-}
-
-
-ReactDOM.render(
-	<AlertProvider template={AlertTemplate} {...options}>
-		<App />
-	</AlertProvider>, document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
